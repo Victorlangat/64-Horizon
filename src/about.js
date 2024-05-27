2b3721d8910848fbc3b167154996c4ed1267a73b
Victorlangat <111707177+Victorlangat@users.noreply.github.com>
2024-05-27 12:40:00 +0300

    this the second commit

--- src/about.js
+++ src/about.js
@@ -36,8 +36,8 @@ function AboutUs(){
     <div className='about-us'>
       <div className='aboutusimage'>
         <div className='aboutusoverlay'>
-          <h2>About Us</h2>
-          <p>We are a team of movie enthusiasts who have created this app to make the process of booking movie tickets easier and more. We hope you enjoy using our app as much as we enjoyed creating it!</p>
+          <h2 className='abouthead'>About Us</h2>
+          <p className='aboutusmessage'>We are a team of movie enthusiasts who have created this app to make the process of booking movie tickets easier and more. We hope you enjoy using our app as much as we enjoyed creating it!</p>
         </div>
       </div>
     </div>
